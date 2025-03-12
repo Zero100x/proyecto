491956b20ca32feebf3b50fae2c9c0a1bee2e906
Diego Ceron <134111842+Zero100x@users.noreply.github.com>
2025-03-12 07:55:45 -0500

    Initial commit

---
+++ portafolio/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className='flex items-center justify-center w-screen h-screen bg-cyan-300'>
+      <h1 className='text-cyan-700'>Diego Ceron</h1> "Diego": Unknown word.
+    </main>
+  );
+}
